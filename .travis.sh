37c744f2773f8914048149f776398a287e068ecb
Evgeny Mandrikov <mandrikov@gmail.com>
2016-11-22 03:54:10 +0100

    Travis: Execute tests with Eclipse 4.6 and 4.7 in addition to 3.8

--- .travis.sh
+++ .travis.sh
@@ -2,12 +2,14 @@
 
 set -euo pipefail
 
-if [[ ${TRAVIS_PULL_REQUEST} == 'false' && ${TRAVIS_BRANCH} == 'master' ]]
+if [[ ${TRAVIS_PULL_REQUEST} == 'false' && ${TRAVIS_BRANCH} == 'master' && ${PLATFORM} == 'e3.8' ]]
 then
   mvn -V -B -e \
-    verify -Djdk.version=1.7 --toolchains=./.travis-toolchains.xml \
-    sonar:sonar -Dsonar.host.url=${SONARQUBE_URL} -Dsonar.login=${SONARQUBE_TOKEN}
+    verify -Djdk.version=${JDK} --toolchains=./.travis-toolchains.xml \
+    sonar:sonar -Dsonar.host.url=${SONARQUBE_URL} -Dsonar.login=${SONARQUBE_TOKEN} \
+    -P ${PLATFORM}
 else
   mvn -V -B -e \
-    verify -Djdk.version=1.7 --toolchains=./.travis-toolchains.xml
+    verify -Djdk.version=${JDK} --toolchains=./.travis-toolchains.xml \
+    -P ${PLATFORM}
 fi
