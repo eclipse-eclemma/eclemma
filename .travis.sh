57de0ea5dfe76e0e315dc00e6ea82e51b0ac67ad
Evgeny Mandrikov <Godin@users.noreply.github.com>
2016-11-22 16:20:24 +0100

    Add UI tests (#8)

--- .travis.sh
+++ .travis.sh
@@ -2,14 +2,19 @@
 
 set -euo pipefail
 
+export DISPLAY=:99.0
+sh -e /etc/init.d/xvfb start
+sleep 5
+metacity --sm-disable --replace 2> metacity.err &
+
 if [[ ${TRAVIS_PULL_REQUEST} == 'false' && ${TRAVIS_BRANCH} == 'master' && ${PLATFORM} == 'e3.8' ]]
 then
   mvn -V -B -e \
     verify -Djdk.version=${JDK} --toolchains=./.travis-toolchains.xml \
     sonar:sonar -Dsonar.host.url=${SONARQUBE_URL} -Dsonar.login=${SONARQUBE_TOKEN} \
-    -P ${PLATFORM}
+    -P ${PLATFORM} -DskipUITests=false
 else
   mvn -V -B -e \
     verify -Djdk.version=${JDK} --toolchains=./.travis-toolchains.xml \
-    -P ${PLATFORM}
+    -P ${PLATFORM} -DskipUITests=false
 fi
---
+++ org.eclipse.eclemma.ui.test/src/org/eclipse/eclemma/ui/ExportImportTest.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright (c) 2006, 2016 Mountainminds GmbH & Co. KG and Contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Evgeny Mandrikov - initial API and implementation
+ *
+ ******************************************************************************/
+package org.eclipse.eclemma.ui;
+
+import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
+import org.junit.After;
+import org.junit.Test;
+
+public class ExportImportTest {
+
+  private static final SWTWorkbenchBot bot = new SWTWorkbenchBot();
+
+  @After
+  public void resetWorkbench() {
+    bot.resetWorkbench();
+  }
+
+  @Test
+  public void testImport() {
+    bot.menu("File").menu("Import...").click();
+    bot.shell("Import").activate();
+
+    SWTBotTreeItem treeItem = bot.tree().getTreeItem("Other").expand();
+    treeItem.getNode("Coverage Session").select();
+    bot.button("Next >").click();
+
+    bot.text(" Please select an existing execution data file.");
+  }
+
+  @Test
+  public void testExport() {
+    bot.menu("File").menu("Export...").click();
+    bot.shell("Export").activate();
+
+    SWTBotTreeItem treeItem = bot.tree().getTreeItem("Java").expand();
+    treeItem.getNode("Coverage Report").select();
+    bot.button("Next >").click();
+
+    bot.text(" No coverage session available for export.");
+  }
+
+}
